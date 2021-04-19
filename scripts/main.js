38f22addd80ce21be4b9e988f4d2763ec0dc138e
Ondřej Tománek <andrewtomanek@gmail.com>
2021-04-20 00:10:14 +0200

    Highlight current link

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,7 @@ document.addEventListener("scroll", hideTitle);
 const burgerButton = document.querySelector(".burger_container");
 
 function openMenu(e) {
+  highlightLink();
   if (navigation.classList.contains("short_nav")) {
     navigation.classList.remove("short_nav");
   }
@@ -78,7 +79,18 @@ function moveDown() {
 if (downButton) {
   downButton.addEventListener("click", moveDown);
 }
-/*smooth scroll*/
+/*Highlight current link*/
+
+function highlightLink() {
+  const navigationLinks = document.querySelectorAll(".nav_link");
+
+  navigationLinks.forEach((link) => {
+    console.log(link);
+    if (link.href === document.URL) link.className += " current-link";
+  });
+}
+
+highlightLink();
 
 /*lightBox*/
 
@@ -133,64 +145,6 @@ function addCloseBtn() {
   closeBtn.addEventListener("click", closeLightbox);
   document.body.appendChild(closeBtn);
 }
-
-/*smooth scroll*/
-
-function init() {
-  new SmoothScroll(document, 300, 40); // target / speed / smoothness
-}
-
-/* function SmoothScroll(target, speed, smooth) {
-  if (target == document)
-    target =
-      document.documentElement || document.body.parentNode || document.body; // cross browser support for document scrolling
-  let moving = false;
-  let pos = target.scrollTop;
-  target.addEventListener("mousewheel", scrolled, false);
-  target.addEventListener("DOMMouseScroll", scrolled, false);
-
-  function scrolled(e) {
-    let delta = e.delta || e.wheelDelta;
-    if (delta === undefined) {
-      //we are on firefox
-      delta = -e.detail;
-    }
-    delta = Math.max(-1, Math.min(1, delta)); // cap the delta to [-1,1] for cross browser consistency
-    pos += -delta * speed;
-    pos = Math.max(0, Math.min(pos, target.scrollHeight - target.clientHeight)); // limit scrolling
-
-    function topClicked() {
-      pos = 0;
-    }
-
-    if (!moving) update();
-
-    topButton.addEventListener("click", topClicked);
-  }
-
-  function update() {
-    moving = true;
-    let delta = (pos - target.scrollTop) / smooth;
-    target.scrollTop += delta;
-    if (Math.abs(delta) > 0.5) requestFrame(update);
-    else moving = false;
-  }
-
-  let requestFrame = (function () {
-    return (
-      window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      function (func) {
-        window.setTimeout(func, 1000 / 50);
-      }
-    );
-  })();
-}
-
-init(); */
 
 /*reveal elements*/
 function displayOnScroll(e) {
